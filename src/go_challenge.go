8463638f0eaec057ee7892afd304d5ca64d64d49
Filipe <filipe@laptop.com>
2020-06-05 18:01:45 +0100

    [go] add get cpu usage function

--- src/go_challenge.go
+++ src/go_challenge.go
@@ -20,6 +20,45 @@ func getSensorData() (int, int, int, int) {
     return sensor1, sensor2, sensor3, sensor4
 }
 
+func getCPUSample() (idle, total uint64) {
+    contents, err := ioutil.ReadFile("/proc/stat")
+    if err != nil {
+        return
+    }
+    lines := strings.Split(string(contents), "\n")
+    for _, line := range(lines) {
+        fields := strings.Fields(line)
+        if fields[0] == "cpu" {
+            numFields := len(fields)
+            for i := 1; i < numFields; i++ {
+                val, err := strconv.ParseUint(fields[i], 10, 64)
+                if err != nil {
+                    fmt.Println("Error: ", i, fields[i], err)
+                }
+                total += val // tally up all the numbers to get total ticks
+                if i == 4 {  // idle is the 5th field in the cpu line
+                    idle = val
+                }
+            }
+            return
+        }
+    }
+    return
+}
+
+func getCPUUsage()(float64){
+    idle0, total0 := getCPUSample()
+    time.Sleep(time.Second)
+    idle1, total1 := getCPUSample()
+
+    idleTicks := float64(idle1 - idle0)
+    totalTicks := float64(total1 - total0)
+    cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+
+    return cpuUsage
+}
+
 func main() {
     s1, s2, s3, s4 := getSensorData()
+    cpu = getCPUUsage()
 }
